ac5947862f973a7d430923f5ad7bb2aa9ce4778a
Kenshin <llwtxzhells11170@gmail.com>
2026-06-04 22:57:43 +0800

    修复middleware拦截logo.svg

--- middleware.ts
+++ middleware.ts
@@ -51,5 +51,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|public|mediapipe|equipment|icons|manifest).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|public|mediapipe|equipment|icons|manifest|logo\\.svg|offline).*)"],
 };
